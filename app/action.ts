3e0642e16ca8ae0db0587cce8ce6f83ef37e0b49
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-02-14 16:49:32 -0800

    Created fetchAnime function to fetch data from api

---
+++ app/action.ts
@@ -0,0 +1,14 @@
+// means that everything here will be run as a server action
+"use server";
+
+// one goal is to call the API and return data
+export const fetchAnime = async (page: number) => {
+  const response = await fetch(
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
+  );
+
+  const data = await response.json();
+
+  console.log(data);
+  return data;
+};
