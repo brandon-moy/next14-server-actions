9f3be0869aa772d0b23b58004089514785e64dcb
Brandon Moy <brandonkaghenmoy@gmail.com>
2024-02-14 17:18:45 -0800

    Removed unecessary comments and imports

--- app/action.tsx
+++ app/action.tsx
@@ -3,7 +3,6 @@
 
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
-// one goal is to call the API and return data
 export const fetchAnime = async (page: number) => {
   const response = await fetch(
     `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
